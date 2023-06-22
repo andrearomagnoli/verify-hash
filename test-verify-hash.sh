d3216e79c8f1612537d065ecbc79571ce0a2b57e
Bruce <brootware@outlook.com>
2023-06-22 10:20:33 +0800

    modified test to only test for hashing algorithms

--- test-verify-hash.sh
+++ test-verify-hash.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 # Test the verify-hash.sh with valid parameters
-result=$(bash verify-hash.sh md5 file.txt 827ccb0eea8a706c4c34a16891f84e7b)
+result=$(bash verify-hash.sh md5 LICENSE 86d3f3a95c324c9479bd8986968f4327)
 if [[ $result = *"OK: Keys match correctly."* ]]; then
-  echo "Test 1: Passed"
+  echo "md5 test: Passed"
 else
-  echo "Test 1: Failed"
+  echo "md5 test: Failed"
 fi
 
-# Test the verify-hash.sh with invalid parameters
-result=$(bash verify-hash.sh md5)
-if [[ $result = *"Illegal parameters: run '$0 -h' for usage."* ]]; then
-  echo "Test 2: Passed"
+result=$(bash verify-hash.sh sha256 LICENSE c71d239df91726fc519c6eb72d318ec65820627232b2f796219e87dcf35d0ab4)
+if [[ $result = *"OK: Keys match correctly."* ]]; then
+  echo "sha256 test: Passed"
 else
-  echo "Test 2: Failed"
+  echo "sha256 test: Failed"
 fi
 
-# Test the verify-hash.sh with help command
-result=$(bash verify-hash.sh -h)
-if [[ $result = *"Usage: $0 <hash> <file> <given_key>"* ]]; then
-  echo "Test 3: Passed"
+result=$(bash verify-hash.sh sha512 LICENSE dc6b68d13b8cf959644b935f1192b02c71aa7a5cf653bd43b4480fa89eec8d4d3f16a2278ec8c3b40ab1fdb233b3173a78fd83590d6f739e0c9e8ff56c282557)
+if [[ $result = *"OK: Keys match correctly."* ]]; then
+  echo "sha512 test: Passed"
 else
-  echo "Test 3: Failed"
+  echo "sha512 test: Failed"
 fi
